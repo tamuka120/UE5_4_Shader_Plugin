2f4b30d7f9d7c589c3c741614308a96619541eb1
tamuka120 <tamuka120@hotmail.com>
2024-09-28 17:31:35 +0100

    Map on disk shader location to virtual shader location on module startup

--- Source/UE5_4_Shader_Plugin/Private/UE5_4_Shader_Plugin.cpp
+++ Source/UE5_4_Shader_Plugin/Private/UE5_4_Shader_Plugin.cpp
@@ -1,18 +1,25 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "UE5_4_Shader_Plugin.h"
+#include "Interfaces/IPluginManager.h"
+
+//#include "Modules/ModuleManager.h"
 
 #define LOCTEXT_NAMESPACE "FUE5_4_Shader_PluginModule"
 
 void FUE5_4_Shader_PluginModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	// Maps the virtual shader source directory to the shader directory in our project
+	const FString pluginBaseDir = IPluginManager::Get().FindPlugin(TEXT("UE5_4_Shader_Plugin"))->GetBaseDir();
+	const FString pluginShaderDir = FPaths::Combine(pluginBaseDir, TEXT("Shaders/Private"));
+	const FString virtualShaderDir = TEXT("/Plugin/UE5_4_Shader_Plugin");
+	AddShaderSourceDirectoryMapping(virtualShaderDir, pluginShaderDir);
 }
 
 void FUE5_4_Shader_PluginModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+	// Cleanup the virtual source directory mapping.
+	ResetAllShaderSourceDirectoryMappings();
 }
 
 #undef LOCTEXT_NAMESPACE
--- Source/UE5_4_Shader_Plugin/Public/UE5_4_Shader_Plugin.h
+++ Source/UE5_4_Shader_Plugin/Public/UE5_4_Shader_Plugin.h
@@ -9,7 +9,20 @@ class FUE5_4_Shader_PluginModule : public IModuleInterface
 {
 public:
 
-	/** IModuleInterface implementation */
+	/**IModuleInterface implementation */
+
+	/** 
+		This code will execute after your module is loaded into memory. The exact timing is specified in the .uplugin file per-module
+	    by the LoadingPhase. This determines when the module is loaded during engine startup.
+
+	    Default: During engine init, after game modules are loaded.
+	    PostEngineInit : After the engine has been initialized.
+	    https://github.com/ibbles/LearningUnrealEngine
+	*/
 	virtual void StartupModule() override;
+
+
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
 	virtual void ShutdownModule() override;
 };
